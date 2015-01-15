b5b2b665bc8182eaaa42f4afb06f3b64a966c538
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2015-01-15 12:41:19 +0100

    added grid hyperconfiguration evaluation

--- python/prediction_svm.py
+++ python/prediction_svm.py
@@ -3,16 +3,28 @@ from sklearn import cross_validation
 import numpy as np
 from sklearn import preprocessing
 import numpy as np
+from operator import itemgetter
 from sklearn.externals import joblib
 import time
 from sklearn.metrics import f1_score
+from sklearn.grid_search import GridSearchCV
 
 name_of_feature_table = "training_features"
 database_name = "stackoverflow"
 user_name = "root"
-password = "password"
-host = "192.168.2.100"
+password = ""
+host = "127.0.0.1"
 
+# Utility function to report best scores
+def report(grid_scores, n_top=3):
+    top_scores = sorted(grid_scores, key=itemgetter(1), reverse=True)[:n_top]
+    for i, score in enumerate(top_scores):
+        print("Model with rank: {0}".format(i + 1))
+        print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+            score.mean_validation_score,
+            np.std(score.cv_validation_scores)))
+        print("Parameters: {0}".format(score.parameters))
+        print("")
 
 def store_svm(clf, file_name):
     joblib.dump(clf, file_name)
@@ -29,7 +41,7 @@ def convert_date_to_timestamp(str):
 def calculate_y(start_date_str, end_date_str):
     start_date = convert_date_to_timestamp(start_date_str)
     end_date = convert_date_to_timestamp(end_date_str)
-    return 1 if int(end_date - start_date) / 60 / 60 < (24 * 6.5) else 0
+    return 1 if int(end_date - start_date) / 60 / 60 < (24 * 2.5) else 0
 
 
 def load_data_from_db():
@@ -39,7 +51,7 @@ def load_data_from_db():
                                   database=database_name,
                                   host=host)
     cursor = cnx.cursor()
-    cursor.execute("Select * From %s" % name_of_feature_table)
+    cursor.execute("Select * From %s Where AnswerDate is not null Or successful = 0" % name_of_feature_table)
     return cursor.fetchall()
 
 
@@ -66,7 +78,45 @@ def train_to_predict_finish(data):
     # store_svm(clf, "output/test.pkl")
 
     print "Scoring Finish-SVM on test data ..."
-    print "Finish-Trainings score: %f" % clf.score(X_test, y_test)  # f1_score(y_test, clf.predict(X_test))
+    print "Finish-Trainings Accuracy: %f F1-score: %f" % (clf.score(X_test, y_test), f1_score(y_test, clf.predict(X_test)))
+    return clf
+
+
+def grid_train_to_predict_time(data):
+    # Prediction of time event
+    filtered_data = filter(lambda x: x[1] == 1, data)
+
+    param_grid = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+        ]
+
+    y = labels_for_time_prediction(filtered_data)
+    unscaled_X = extract_features(filtered_data)
+
+    clf = svm.SVC()  # probability = True)
+
+    X_train_unscaled, X_test_unscaled, y_train, y_test = cross_validation.train_test_split(unscaled_X, y,
+                                                                                           test_size=1000,
+                                                                                           train_size=10000,
+                                                                                           random_state=0)
+
+    scaler = preprocessing.StandardScaler().fit(X_train_unscaled)
+
+    X_train = scaler.transform(X_train_unscaled)
+    X_test = scaler.transform(X_test_unscaled)
+
+    print "Training SVM on trainings data to estimate answer time..."
+    grid_search = GridSearchCV(clf, param_grid=param_grid, refit=True, n_jobs=4)
+    grid_search.fit(X_train, y_train)
+
+    print("GridSearchCV for %d candidate parameter settings." % len(grid_search.grid_scores_))
+    report(grid_search.grid_scores_)
+
+    # store_svm(clf, "output/test.pkl")
+
+    print "Scoring Time-SVM on test data ..."
+    print "Time-Trainings score: %f" % grid_search.score(X_test, y_test)  # f1_score(y_test, clf.predict(X_test))
     return clf
 
 
@@ -95,9 +145,8 @@ def train_to_predict_time(data):
     # store_svm(clf, "output/test.pkl")
 
     print "Scoring Time-SVM on test data ..."
-    print "Time-Trainings score: %f" % clf.score(X_test, y_test)  # f1_score(y_test, clf.predict(X_test))
+    print "Time-Trainings Accuracy: %f F1-score: %f" % (clf.score(X_test, y_test), f1_score(y_test, clf.predict(X_test)))
     return clf
-
 
 def extract_features(data):
     X = []
@@ -105,7 +154,7 @@ def extract_features(data):
     # Drop the Id field and the y value. This assumes a row looks like this: Id, Finished, StartDate, EndDate, feature1, feature2, feature3, ...
     for row in data:
         X.append([x for idx, x in enumerate(row) if
-                  idx > 3])  # remove the first three elements from the column (id and dates)
+                  idx > 14])  # remove the first three elements from the column (id and dates)
     return X
 
 
